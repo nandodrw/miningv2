bcfa1a8e3260ce1ab59ae29dcb1bbad9c63760ef
Fernando Catacora <nando.drw@gmail.com>
2015-05-01 16:35:51 -0500

    finish v1 of facebook-handler

--- scripts/facebook-handler.js
+++ scripts/facebook-handler.js
@@ -3,13 +3,6 @@
 
 (function () {
 
-  //creating module variables
-  var likeList = [];
-  var likedFeed = {};
-  var likedPhotos = {};
-
-
-
   function initialize(obj, callback) {
       if(obj.appId  && obj.version){
         window.fbAsyncInit = callback;
@@ -51,11 +44,9 @@
   }
 
   function feedAllLikedContent(callback){
-
     var pointer = {
       data: []
     };
-
     getLikedContent(function(response){
       if(pointer &&
         response.likes &&
@@ -110,9 +101,7 @@
   }
 
   function getNodePostedContent(node, callback) {
-
     var route = buildNodeRequest(node)
-
     FB.api(route, 'get', function(response){
       callback(response);
     });
@@ -159,94 +148,6 @@
     }
     batchCall(params, callback);
   }
-
-  function addMetaData (data,type){
-    if(type == 'feed'){
-      for(i in data){
-        if(data[i].likes){
-          data[i].likes_count = data[i].likes.summary.total_count;
-        } else {
-          data[i].likes_count = 0;
-        };
-        if(data[i].comments){
-          data[i].comments_count = data[i].comments.summary.total_count;
-        } else {
-          data[i].comments_count = 0;
-        };
-        if(data[i].shares){
-          data[i].shares_count = data[i].shares.count;
-        } else {
-          data[i].shares_count = 0;
-        };
-      };
-    } else if (type == 'photos'){
-        for(i in data){
-          if(data[i].likes){
-            data[i].likes_count = data[i].likes.summary.total_count;
-          } else {
-            data[i].likes_count = 0;
-          };
-          if(data[i].comments){
-            data[i].comments_count = data[i].comments.summary.total_count;
-          } else {
-            data[i].comments_count = 0;
-          };
-        };
-      }
-    };
-
-  function statusChangeCallback (response){
-    if (response.status === 'connected') {
-      $('#general-message').text('Welcome!!');
-      FBhandler.status = 'connected';
-      return 'connected'
-    } else if (response.status === 'not_authorized') {
-      $('#general-message').text('Please log  into this app.');
-      FBhandler.status = 'not_authorized'
-      return 'not_authorized'
-    } else {
-      $('#general-message').text('Please log into Facebook.');
-      FBhandler.status = 'not-logged'
-      return 'not-logged'
-    }
-  };
-
-  function callMe () {
-    return FBauxiliar.callFbGraph("/me");
-  };
-
-  function callLikes (callback){
-    var likeObj = {};
-    var callRemote = callback;
-    FBauxiliar.callFbGraph('me?fields=likes.fields(id,name,category,likes)', function(likeObj){
-      if(likeObj.likes){
-        FBhandler.likeList = FBhandler.likeList.concat(likeObj.likes.data);
-        if(likeObj.likes.paging){
-          FBauxiliar.pagingLikes(likeObj.likes.paging.next,callRemote);
-        }
-      } else {
-        callback({error : 'No liked pages were grabbed'});
-      }
-
-    });
-  };
-
-  function getFeedNextPage (node,callback){
-
-    if(FBhandler.likedFeed[node].paging.next){
-      $.get(FBhandler.likedFeed[node].paging.next,function(response){
-        if(response.error){
-          if(callback){callback(response);}
-        }else{
-          FBauxiliar.addMetaData(response.data,'feed');
-          FBauxiliar.storeFeed(node,response);
-          if (callback) {callback(response.data);};
-        }
-
-      });
-    } else {return false};
-
-  };
 
   // defining global object
   window.FBhandler = {};
